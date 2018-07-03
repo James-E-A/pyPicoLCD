52360ed85be0a2daf156ba906859aa576baf80da
James Edington <james.edington@peraton.com>
2018-07-03 02:13:30 -0500

    Various prunings

--- PicoLCD.py
+++ PicoLCD.py
@@ -1,7 +1,6 @@
 import usb.core,usb.util
 import time
 
-OUT_REPORT_LED_STATE            = 0x81
 OUT_REPORT_LCD_BACKLIGHT        = 0x91
 OUT_REPORT_LCD_CONTRAST         = 0x92
 OUT_REPORT_LCD_CONTROL          = 0x93
@@ -14,36 +13,19 @@ OUT_REPORT_DATA			= 0x95
 OUT_REPORT_CMD_DATA		= 0x96
 
 class PicoLcd:
-	def __init__(self):
-		self._dev=usb.core.find(idVendor=0x04d8, idProduct=0xc002)
+	def __init__(self, idVendor=0x04d8, idProduct=0xc002):
+		self._dev=usb.core.find(idVendor=idVendor, idProduct=idProduct)
 		try:
 			self._dev.detach_kernel_driver(0)
 			self._dev.set_configuration()
 			usb.util.claim_interface(self._dev,0)
 			self._dev.set_interface_altsetting(0)
 		except usb.core.USBError:
-			pass
-#		self._handle=usb.util.find_descriptor(
-#		 self._dev.get_active_configuration()[(0,0)],
-#		 custom_match = \
-#		  lambda e: \
-#		   usb.util.endpoint_direction(e.bEndpointAddress) \
-#		   == \
-#		   usb.util.ENDPOINT_OUT
-#		)
-#		assert self._handle is not None
+			pass #TODO throw back actual errors
 	
 	def write(self, data):
 		print(repr(data))
 		return self._dev.write(usb.util.ENDPOINT_OUT+1, data)
-	
-	def draw_text(self, text, row=0, col=0):
-		addr = [0x80,0xc0,0x94,0xd4][row] + col
-		self.write(bytes([
-		 0x94,0x00,0x01,0x00,0x64,addr,
-		 0x95,0x01,0x00,0x01,
-		 len(text),*text.encode('ascii')
-		]))
 	
 	def clear(self): #drv_pLG_clear from drv_picoLCDGraphic.c
 		self.write(b'\x93\x01\x00')
@@ -91,35 +73,29 @@ class PicoLcd:
 					cmd4[5+(index-32)] = temp
 				self.write(cmd4)
 
-	def LEGACY_clear(self):
-		for row in range(4):
-			self.draw_text(row, 0, " " * 20)
 	
 	def set_backlight(self, brightness):
-		return self.write(bytes([0x91, brightness]))
+		return self.write(bytes([
+		 OUT_REPORT_LCD_BACKLIGHT,
+		 brightness
+		]))
 	
-	def LEGACY_leds(self, state):
-		self.wr(bytes(OUT_REPORT_LED_STATE, state))
-	
-	def LEGACY_flash(self):
-		for brightness in range(20, 0, -1) + range(0, 21):
-			self.backlight(brightness)
-			time.sleep(.01)
-
-	def text2(self, text, row=0,col=0): #drv_pL_write from drv_PicoLCD.c
-		Q=bytearray()
-		Q.append(0x98)
-		Q.append(row)
-		Q.append(col)
-		Q.append(len(text))
-		for char in text:
-			Q.append(char)
-		return self.write(bytes(Q))
+	def text(self, text, row=0,col=0): #drv_pL_write from drv_PicoLCD.c
+		# https://lcd4linux.bulix.org/browser/trunk/drv_picoLCD.c#L250
+		# I have no idea why this isn't working...
+		return self.write(bytes([
+		 OUT_REPORT_LCD_TEXT,
+		 row,
+		 col,
+		 len(text),
+		 *text
+		]))
 
 if __name__ == "__main__":
 	from datetime import datetime
 	p = PicoLcd()
 	p.clear()
-	for i in range(256):
-		p.set_backlight(i)
+	for i in range(8):
+		p.set_backlight(1<<i)
+		p.text("HELLO 123 TESTING TESTING 123".encode('ascii'), i, 5)
 		time.sleep(0.2)
